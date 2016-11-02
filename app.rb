76baa45ca2368a534bd259177f623168c2eaf28e
AndreiLukashevich <rid7003482@gmail.com>
2016-11-02 17:42:51 +0300

    Add Barbers migration

--- app.rb
+++ app.rb
@@ -2,13 +2,15 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sinatra/activetecord'
+require 'sinatra/activerecord'
+
 
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
+end
 
-
+class Barber < ActiveRecord::Base
 end
 
 get '/' do
